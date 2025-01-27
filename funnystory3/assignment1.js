0e1db3e8aa0272ed0e6195dad461de7ffacee014
Joseph Hutchinson <joseph.hutch@gmail.com>
2025-01-27 14:35:15 -0800

    Add files via upload

---
+++ funnystory3/assignment1.js
@@ -0,0 +1,31 @@
+console.log("variables");
+//1. Declare Variables
+// Using let
+let character = "Sam";
+let action = "scream";
+let object ="several giant dogs";
+let place = " the grocery store";
+let time = "7 am";
+
+//using "const"
+const occasion = " a giant robot dance party";
+
+//2. create the story
+console.log("---Funny Story---");
+console.log(character + " went to" + place + " at " + time + ".");
+console.log("Suddenly " + character + " saw " + object + " and decided to " + action + ".");
+console.log("It turned out to be" + occasion + " and everyone had an amazing time!");
+
+// New version of the story
+console.log("---Different version of the story---");
+
+// Change values to create a different story
+character2 = "Linda";
+action = "laugh";
+object =" a giant clown";
+place = " the buffet";
+time = "10 am";
+
+console.log(character2 + " went to" + place + " at " + time + ".");
+console.log("Suddenly " + character2 + " saw " + object + " and decided to " + action + ".");
+console.log("It turned out to be" + occasion + " and everyone had an amazing time!");
